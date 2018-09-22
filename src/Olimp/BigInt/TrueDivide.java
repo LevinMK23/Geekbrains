23fdfecdb08d226012b1bc7e8aa9c02a34c34e74
LevinMK23 <levinMK23@gmail.com>
2018-09-22 12:47:02 +0300

    second commit

--- src/Olimp/BigInt/TrueDivide.java
+++ src/Olimp/BigInt/TrueDivide.java
@@ -7,7 +7,37 @@ import java.util.Scanner;
 public class TrueDivide {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
-        BigDecimal a = new BigDecimal(in.next());
-        System.out.println(a.pow(0.5));
+        BigInteger a = new BigInteger(in.next());
+        if(a.equals(BigInteger.ZERO)){
+            System.out.print(0);
+            return;
+        }
+        if(a.equals(BigInteger.ONE)){
+            System.out.print(1);
+            return;
+        }
+        BigInteger hi = new BigInteger("10").pow(1501);
+        BigInteger lo = BigInteger.ZERO;
+        while (hi.add(lo.negate()).compareTo(BigInteger.ZERO) > 0){
+            BigInteger mid = lo.add(hi).divide(new BigInteger("2"));
+            if (mid.pow(2).compareTo(a) == 0){
+                System.out.print(mid.toString());
+                return;
+            }
+            if (mid.pow(2).compareTo(a) > 0){
+                if (mid.add(BigInteger.ONE.negate()).pow(2).compareTo(a) < 0){
+                    System.out.print(mid.add(BigInteger.ONE.negate()).toString());
+                    return;
+                }
+                else hi = mid;
+            }
+            else if(mid.pow(2).compareTo(a) < 0){
+                if (mid.add(BigInteger.ONE).pow(2).compareTo(a) > 0){
+                    System.out.print(mid.toString());
+                    return;
+                }
+                else lo = mid;
+            }
+        }
     }
 }
